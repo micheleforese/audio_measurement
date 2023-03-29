ccc229f9fd2429bd74b2dea2218e7c2a701c968f
Michele Forese <michele.forese.personal@gmail.com>
2023-03-29 14:13:43 +0200

    added variable for db auth data

--- audio/constant.py
+++ audio/constant.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
-APP_HOME = Path(__file__).parent.parent
-APP_TEST = APP_HOME / "test"
+APP_HOME: Path = Path(__file__).parent.parent
+APP_TEST: Path = APP_HOME / "test"
 APP_TEST.mkdir(exist_ok=True, parents=True)
 
-APP_AUDIO_TEST = APP_HOME / "audio-test"
+APP_AUDIO_TEST: Path = APP_HOME / "audio-test"
 
-APP_LOGGING_FILE = APP_HOME / "logging/app.log"
+APP_LOGGING_FILE: Path = APP_HOME / "logging/app.log"
+
+APP_DB_AUTH_PATH: Path = APP_HOME / "configuration" / "db.auth.json"
