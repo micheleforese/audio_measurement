8f171186204d672269b6f11d0c4614773f289d85
Michele Forese <michele.forese.personal@gmail.com>
2023-03-29 14:12:53 +0200

    refactoring Timer class

--- audio/utility/timer.py
+++ audio/utility/timer.py
@@ -1,49 +1,37 @@
 import time
 from datetime import timedelta
-
-from rich.panel import Panel
-
-from audio.console import console
+from typing import Self
 
 
 class TimerError(Exception):
     """A custom exception used to report errors in use of Timer class"""
 
 
-class Timer_Message:
-    elapsed_time: timedelta
-    message: str
+class TimerAlreadyStartedError(TimerError):
+    """Timer already started."""
 
-    def __init__(self, elapsed_time, message) -> None:
-        self.elapsed_time = elapsed_time
-        self.message = message
 
-    def __repr__(self) -> str:
-        return f"[green]{self.message}[/green]: [blue]{self.elapsed_time}[/blue]"
-
-    def print(self):
-        console.print(
-            Panel(f"[green]{self.message}[/green]: [blue]{self.elapsed_time}[/blue]"),
-        )
+class TimerNotStartedError(TimerError):
+    """Timer didn't start."""
 
 
 class Timer:
     _start_time = None
     _last_lap = None
 
-    def start(self):
+    def start(self: Self) -> float:
         """Start a new timer"""
         if self._start_time is not None:
-            raise TimerError("Timer is running. Use .stop() to stop it")
+            raise TimerAlreadyStartedError
 
         self._start_time = time.perf_counter()
         self._last_lap = self._start_time
         return self._start_time
 
-    def stop(self):
+    def stop(self: Self) -> timedelta:
         """Stop the timer, and report the elapsed time"""
         if self._start_time is None:
-            raise TimerError("Timer is not running. Use start() to start it")
+            raise TimerNotStartedError
 
         elapsed_time: timedelta = timedelta(
             seconds=time.perf_counter() - self._start_time,
@@ -52,8 +40,10 @@ class Timer:
         self._start_time = None
         return elapsed_time
 
-    def lap(self):
-        current_time = time.perf_counter()
-        diff = current_time - self._last_lap
+    def lap(self: Self) -> timedelta:
+        current_time: float = time.perf_counter()
+        if self._last_lap is None:
+            raise TimerNotStartedError
+        diff: float = current_time - self._last_lap
         self._last_lap = current_time
         return timedelta(seconds=diff)
